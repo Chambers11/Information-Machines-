566baea0f29c36968737d0d4f45c987dc8de7961
Chambers <964089@swansea.ac.uk>
2021-05-15 20:37:08 +0100

    Readable RFID tags be each machine established, numbers on palette changed or  reflect palette number in reality.

--- UnityInformationMachines/Assets/Scripts/InterfaceMachine.cs
+++ UnityInformationMachines/Assets/Scripts/InterfaceMachine.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using game4automation;
 using System;
+using UnityEngine.UI; 
 
 public class InterfaceMachine : MonoBehaviour
 {
     private OPCUA_Interface oPCUA_Interface;
     private TrackingPalettes trackingPalettes;
 
-
-
-    public string NodeID;
+    public Text paletteNumber; 
+    
+    public string NodeID, NodeValue;
 
 
     public Transform conveyerStartTrans;
@@ -42,6 +43,11 @@ public class InterfaceMachine : MonoBehaviour
     private void NodeChanged(OPCUANodeSubscription sub, object value)
     {
         Debug.Log(value.ToString());
+        NodeValue = value.ToString();
+
+
+        paletteNumber.text = NodeValue; 
+
         int paletteID = Int32.Parse(value.ToString());
         trackingPalettes.Palettes[paletteID].transform.position = conveyerStartTrans.position;
         trackingPalettes.Palettes[paletteID].GetComponent<Palette>().Move(conveyerEndTrans.position);
